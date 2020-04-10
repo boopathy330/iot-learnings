ebad9cb23024434c83ac07932cfbc127757b6e33
Harinandhan23 <beetleshacks@gmail.com>
2020-04-10 09:10:49 +0530

    Updated Json library & Serial response.

---
+++ hardware/HW_code.ino
@@ -0,0 +1,83 @@
+#include "DHT.h"
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
+#define DHTPIN 2
+
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+float humidityData;
+float temperatureData;
+
+const char *ssid = "ACT"; //
+const char *password = "8056563551";
+//WiFiClient client;
+char server[] = "192.168.0.102"; //eg: 192.168.0.222
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(10);
+  dht.begin();
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password); //connect to wifi router
+                              //wait for connection
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  ////If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Start the server
+  //server.begin();
+  Serial.println("Server started");
+  Serial.print(WiFi.localIP());
+  delay(1000);
+  Serial.println("connecting...");
+}
+void loop()
+{
+  humidityData = dht.readHumidity();
+  temperatureData = dht.readTemperature();
+  sendingDataToBackend();
+  delay(30000); // interval
+}
+
+void sendingDataToBackend() //CONNECTING WITH MYSQL
+{
+  DynamicJsonBuffer jb;
+  JsonVariant bodyJson = jb.createObject();
+  bodyJson["id"] = 1231;
+  bodyJson["temperature"] = temperatureData;
+  bodyJson["humidity"] = humidityData;
+  String bodyString = "";
+  bodyJson.printTo(bodyString);
+  HTTPClient http;
+  WiFiClient client;
+  http.begin(client,"http://192.168.0.104:3000/roomLevel");
+  http.addHeader("Content-Type", "application/json");
+   Serial.print("------------body-----------");
+    Serial.print(bodyString);
+  int httpCode = http.POST(bodyString);
+  Serial.print("---------------response code-------------");
+  Serial.print(httpCode);
+  (httpCode > 0 && httpCode == 200)
+      ? Serial.print("data send")
+      : Serial.print("data send failed");
+  http.end();
+}
